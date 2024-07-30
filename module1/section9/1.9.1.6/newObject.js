e72df6f6c13339960dc517b5615bb79082515907
vhtc <victor.tuy@hotmail.com>
2024-07-30 09:59:18 -0300

    new Object()

---
+++ module1/section9/1.9.1.6/newObject.js
@@ -0,0 +1,8 @@
+// Duas maneiras de se criar objeto
+
+
+let objetoBranco = new Object()
+console.log(objetoBranco.constructor.name)
+
+let outroObjeto = {}
+console.log(outroObjeto.constructor.name)
---
+++ module1/section9/1.9.1.7/object.create.js
@@ -0,0 +1,5 @@
+let objetoBranco = Object.create(null)
+
+console.log(typeof objetoBranco.constructor)
+
+console.log(objetoBranco)
