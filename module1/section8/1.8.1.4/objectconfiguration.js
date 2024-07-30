ad4d938b2914f2446023cf0d9f1a9688552e056b
vhtc <victor.tuy@hotmail.com>
2024-07-30 09:16:26 -0300

    object configuration

---
+++ module1/section8/1.8.1.4/objectconfiguration.js
@@ -0,0 +1,9 @@
+/*
+
+
+    Object.preventExtensions(obj)– Depois de chamar este método, não seremos capazes de adicionar novas propriedades ao objeto obj;
+    Object.seal(obj)– não permite a adição, remoção ou reconfiguração das propriedades do objeto obj;
+    Object.freeze(obj)– semelhante a Object.seal, mas também torna impossível alterar o valor da propriedade.
+
+
+*/
